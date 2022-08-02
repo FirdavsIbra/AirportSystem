9f046db9b60e3e2d677c0422ed0e5c488335331b
Rashidovicc <muhammmadibnabdurashid@gmail.com>
2022-08-02 11:58:59 +0300

    GetHash

--- AirportSystem.Service/Interfaces/IBlackListService.cs
+++ AirportSystem.Service/Interfaces/IBlackListService.cs
@@ -1,7 +1,18 @@
-﻿namespace AirportSystem.Service.Interfaces
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using AirportSystem.Domain.Entities.BlackLists;
+using AirportSystem.Service.DTO_s.BlackLists;
+
+namespace AirportSystem.Service.Interfaces
 {
     public interface IBlackListService
     {
-        
+        Task<BlackList> CreateAsync(BlackListForCreation blackForCreation);
+        Task<BlackList> UpdateAsync(long id, BlackListForCreation airplaneForCreation);
+        Task<bool> DeleteAsync(Expression<Func<BlackList, bool>> expression);
+        Task<IEnumerable<BlackList>> GetAllAsync(Expression<Func<BlackList, bool>> expression = null);
+        Task<BlackList> GetAsync(Expression<Func<BlackList, bool>> expression);
     }
 }
